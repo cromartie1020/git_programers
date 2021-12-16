c95395a976a5fabbc58bdd14b644e2b3f5bf6fdd
cromartie1020 <hrc245@gmail.com>
2021-12-16 15:45:06 -0500

    Added a program Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("I just added this in Visual Studio");
+        Console.WriteLine("I just added this in the command line repo");
+    }
+}
+
